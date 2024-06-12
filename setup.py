4e4f37201fd76ea379d3bfd9fc510bdf38996d89
kmarov17 <kodanadiarra456@gmail.com>
2024-06-12 21:37:26 +0000

    Add .gitignore file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi_project_initializer',
-    version='0.1.0',
+    version='0.1.2',
     packages=find_packages(),
     install_requires=[
         'gitpython',
